6c6ae382cc470610478ea7cc6afcc358be431718
Jussi Mitteli <jussi.mitteli@gmail.com>
2026-01-16 20:48:29 +0200

    Calculator engine code added. Still under development

--- calculatorengine.h
+++ calculatorengine.h
@@ -8,11 +8,28 @@ class calculatorengine
 {
 public:
     calculatorengine();
-    void clear();
-    void inputDigit(int digit);
-    void pressPlus();
-    void pressEquals();
-    QString displayText()const;
+    void clear();//clears the display
+    void inputDigit(int digit);//Adds one number to current input 0...9
+    void pressPlus();//User presses plus and record the operand
+    void pressMinus();//User presses minus and record the operand
+    void pressMuiltiply();//User presses multiply and record the operand
+    void pressDivide();//User presses divide and record the operand
+    void pressEquals();//calculates up to the last operand
+
+    QString displayText()const;//Returns the text on the display
+
+private:
+    QString m_entryText = "0";//Display text/user input
+
+    bool m_startNewEntry = true;//if true will begin a new number entry
+
+    double m_accumulator = 0.0;
+
+
+
+
+
+
 };
 
 #endif // CALCULATORENGINE_H
